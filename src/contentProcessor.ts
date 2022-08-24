280957667f6eb659646cc291acaa501839e16b6e
hzy <1402469615@qq.com>
2022-08-24 09:38:52 +0800

    support path pattern

--- src/contentProcessor.ts
+++ src/contentProcessor.ts
@@ -1,128 +1,135 @@
 import { URL } from "url";
 import path from "path";
 
-import { App, DataAdapter } from "obsidian";
+import {App, DataAdapter, normalizePath, TFile} from "obsidian";
 
 import {
-  isUrl,
-  downloadImage,
-  fileExtByContent,
-  cleanFileName,
-  pathJoin,
+    isUrl,
+    downloadImage,
+    fileExtByContent,
+    cleanFileName,
+    pathJoin,
+    getAttachmentFolderPath,
+    getAttachmentFolderFullPath
 } from "./utils";
 import {
-  FILENAME_TEMPLATE,
-  MAX_FILENAME_INDEX,
-  FILENAME_ATTEMPTS,
+    FILENAME_TEMPLATE,
+    MAX_FILENAME_INDEX,
+    FILENAME_ATTEMPTS,
 } from "./config";
 import { linkHashes } from "./linksHash";
 
-export function imageTagProcessor(app: App, mediaDir: string) {
-  async function processImageTag(match: string, anchor: string, link: string) {
-    if (!isUrl(link)) {
-      return match;
-    }
-
-    try {
-      const fileData = await downloadImage(link);
-
-      // when several images refer to the same file they can be partly
-      // failed to download because file already exists, so try to resuggest filename several times
-      let attempt = 0;
-      while (attempt < FILENAME_ATTEMPTS) {
-        try {
-          const { fileName, needWrite } = await chooseFileName(
-            app.vault.adapter,
-            mediaDir,
-            anchor,
-            link,
-            fileData
-          );
-
-          if (needWrite && fileName) {
-            await app.vault.createBinary(fileName, fileData);
-          }
-
-          if (fileName) {
-            return `![${anchor}](${fileName})`;
-          } else {
+export function imageTagProcessor(app: App, attachmentPath: string, mdFile: TFile) {
+    async function processImageTag(match: string, anchor: string, link: string) {
+        if (!isUrl(link)) {
             return match;
-          }
-        } catch (error) {
-          if (error.message === "File already exists.") {
-            attempt++;
-          } else {
-            throw error;
-          }
         }
-      }
-      return match;
-    } catch (error) {
-      console.warn("Image processing failed: ", error);
-      return match;
-    }
-  }
 
-  return processImageTag;
+        try {
+            const fileData = await downloadImage(link);
+
+            // when several images refer to the same file they can be partly
+            // failed to download because file already exists, so try to resuggest filename several times
+            let attempt = 0;
+            while (attempt < FILENAME_ATTEMPTS) {
+                try {
+                    const {fileName, needWrite} = await chooseFileName(
+                        app.vault.adapter,
+                        getAttachmentFolderFullPath(mdFile.parent.path, mdFile.basename, attachmentPath),
+                        anchor,
+                        link,
+                        fileData
+                    );
+
+                    if (needWrite && fileName) {
+                        await app.vault.createBinary(fileName, fileData);
+                    }
+
+                    if (fileName) {
+                        let filePath = fileName
+                        let pp = mdFile.parent.path.startsWith("/") ? mdFile.parent.path.slice(1) : mdFile.parent.path;
+                        if (!path.isAbsolute(attachmentPath)) {
+                            filePath = normalizePath("./" + filePath.slice(pp.length))
+                        }
+                        return `![${anchor}](${filePath})`;
+                    } else {
+                        return match;
+                    }
+                } catch (error) {
+                    if (error.message === "File already exists.") {
+                        attempt++;
+                    } else {
+                        throw error;
+                    }
+                }
+            }
+            return match;
+        } catch (error) {
+            console.warn("Image processing failed: ", error);
+            return match;
+        }
+    }
+
+    return processImageTag;
 }
 
 async function chooseFileName(
-  adapter: DataAdapter,
-  dir: string,
-  baseName: string,
-  link: string,
-  contentData: ArrayBuffer
+    adapter: DataAdapter,
+    dir: string,
+    baseName: string,
+    link: string,
+    contentData: ArrayBuffer
 ): Promise<{ fileName: string; needWrite: boolean }> {
-  const fileExt = await fileExtByContent(contentData);
-  if (!fileExt) {
-    return { fileName: "", needWrite: false };
-  }
-  // if there is no anchor try get file name from url
-  if (!baseName) {
-    const parsedUrl = new URL(link);
+    const fileExt = await fileExtByContent(contentData);
+    if (!fileExt) {
+        return {fileName: "", needWrite: false};
+    }
+    // if there is no anchor try get file name from url
+    if (!baseName) {
+        const parsedUrl = new URL(link);
 
-    baseName = path.basename(parsedUrl.pathname);
-  }
-  // if there is no part for file name from url use name template
-  if (!baseName) {
-    baseName = FILENAME_TEMPLATE;
-  }
-
-  // if filename already ends with correct extension, remove it to work with base name
-  if (baseName.endsWith(`.${fileExt}`)) {
-    baseName = baseName.slice(0, -1 * (fileExt.length + 1));
-  }
-
-  baseName = cleanFileName(baseName);
-
-  let fileName = "";
-  let needWrite = true;
-  let index = 0;
-  while (!fileName && index < MAX_FILENAME_INDEX) {
-    const suggestedName = index
-      ? pathJoin(dir, `${baseName}-${index}.${fileExt}`)
-      : pathJoin(dir, `${baseName}.${fileExt}`);
-
-    if (await adapter.exists(suggestedName, false)) {
-      linkHashes.ensureHashGenerated(link, contentData);
-
-      const fileData = await adapter.readBinary(suggestedName);
-
-      if (linkHashes.isSame(link, fileData)) {
-        fileName = suggestedName;
-        needWrite = false;
-      }
-    } else {
-      fileName = suggestedName;
+        baseName = path.basename(parsedUrl.pathname);
+    }
+    // if there is no part for file name from url use name template
+    if (!baseName) {
+        baseName = FILENAME_TEMPLATE;
     }
 
-    index++;
-  }
-  if (!fileName) {
-    throw new Error("Failed to generate file name for media file.");
-  }
+    // if filename already ends with correct extension, remove it to work with base name
+    if (baseName.endsWith(`.${fileExt}`)) {
+        baseName = baseName.slice(0, -1 * (fileExt.length + 1));
+    }
 
-  linkHashes.ensureHashGenerated(link, contentData);
+    baseName = cleanFileName(baseName);
 
-  return { fileName, needWrite };
+    let fileName = "";
+    let needWrite = true;
+    let index = 0;
+    while (!fileName && index < MAX_FILENAME_INDEX) {
+        const suggestedName = index
+            ? pathJoin(dir, `${baseName}-${index}.${fileExt}`)
+            : pathJoin(dir, `${baseName}.${fileExt}`);
+
+        if (await adapter.exists(suggestedName, false)) {
+            linkHashes.ensureHashGenerated(link, contentData);
+
+            const fileData = await adapter.readBinary(suggestedName);
+
+            if (linkHashes.isSame(link, fileData)) {
+                fileName = suggestedName;
+                needWrite = false;
+            }
+        } else {
+            fileName = suggestedName;
+        }
+
+        index++;
+    }
+    if (!fileName) {
+        throw new Error("Failed to generate file name for media file.");
+    }
+
+    linkHashes.ensureHashGenerated(link, contentData);
+
+    return {fileName, needWrite};
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,69 +1,99 @@
 import path from "path";
 import got from "got";
-import { fromBuffer } from "file-type";
+import {fromBuffer} from "file-type";
 import isSvg from "is-svg";
 import filenamify from "filenamify";
 
-import { DIRTY_IMAGE_TAG, FORBIDDEN_SYMBOLS_FILENAME_PATTERN } from "./config";
+import {DIRTY_IMAGE_TAG, FORBIDDEN_SYMBOLS_FILENAME_PATTERN} from "./config";
+import {normalizePath} from "obsidian";
+import is from "@sindresorhus/is";
+import string = is.string;
+
+class TemplateString extends String {
+    interpolate(params: Object) {
+        const names = Object.keys(params);
+        const vals = Object.values(params);
+        return new Function(...names, `return \`${this}\`;`)(...vals);
+    }
+}
+
+export function getAttachmentFolderPath(attachmentPath: string, mdFileName: string) {
+    let path = new TemplateString(attachmentPath).interpolate({
+        filename: mdFileName
+    });
+    return path;
+}
+
+export function getAttachmentFolderFullPath(mdParentPath: string, mdFileName: string, attachmentPath: string) {
+    let attachmentFolder = '';
+
+    if (!path.isAbsolute(attachmentPath))
+        attachmentFolder = path.join(mdParentPath, getAttachmentFolderPath(attachmentPath, mdFileName));
+    else {
+        attachmentFolder = getAttachmentFolderPath(attachmentPath, mdFileName);
+    }
+    return normalizePath(attachmentFolder);
+}
+
 /*
 https://stackoverflow.com/a/48032528/1020973
 It will be better to do it type-correct.
 
 */
 export async function replaceAsync(str: any, regex: any, asyncFn: any) {
-  const promises: Promise<any>[] = [];
-  str.replace(regex, (match: string, ...args: any) => {
-    const promise = asyncFn(match, ...args);
-    promises.push(promise);
-  });
-  const data = await Promise.all(promises);
-  return str.replace(regex, () => data.shift());
+    const promises: Promise<any>[] = [];
+    str.replace(regex, (match: string, ...args: any) => {
+        const promise = asyncFn(match, ...args);
+        promises.push(promise);
+    });
+    const data = await Promise.all(promises);
+    return str.replace(regex, () => data.shift());
 }
 
 export function isUrl(link: string) {
-  try {
-    return Boolean(new URL(link));
-  } catch (_) {
-    return false;
-  }
+    try {
+        return Boolean(new URL(link));
+    } catch (_) {
+        return false;
+    }
 }
 
 export async function downloadImage(url: string): Promise<ArrayBuffer> {
-  const res = await got(url, { responseType: "buffer" });
-  return res.body;
+    const res = await got(url, {responseType: "buffer"});
+    return res.body;
 }
 
 export async function fileExtByContent(content: ArrayBuffer) {
-  const fileExt = (await fromBuffer(content))?.ext;
+    const fileExt = (await fromBuffer(content))?.ext;
 
-  // if XML, probably it is SVG
-  if (fileExt == "xml") {
-    const buffer = Buffer.from(content);
-    if (isSvg(buffer)) return "svg";
-  }
+    // if XML, probably it is SVG
+    if (fileExt == "xml") {
+        const buffer = Buffer.from(content);
+        if (isSvg(buffer)) return "svg";
+    }
 
-  return fileExt;
+    return fileExt;
 }
 
 function recreateImageTag(match: string, anchor: string, link: string) {
-  return `![${anchor}](${link})`;
+    return `![${anchor}](${link})`;
 }
 
 export function cleanContent(content: string) {
-  const cleanedContent = content.replace(DIRTY_IMAGE_TAG, recreateImageTag);
-  return cleanedContent;
+    const cleanedContent = content.replace(DIRTY_IMAGE_TAG, recreateImageTag);
+    return cleanedContent;
 }
 
 export function cleanFileName(name: string) {
-  const cleanedName = filenamify(name).replace(
-    FORBIDDEN_SYMBOLS_FILENAME_PATTERN,
-    "_"
-  );
-  return cleanedName;
+    const cleanedName = filenamify(name).replace(
+        FORBIDDEN_SYMBOLS_FILENAME_PATTERN,
+        "_"
+    );
+    return cleanedName;
 }
 
 export function pathJoin(dir: string, subpath: string): string {
-  const result = path.join(dir, subpath);
-  // it seems that obsidian do not understand paths with backslashes in Windows, so turn them into forward slashes
-  return result.replace(/\\/g, "/");
+    const result = path.join(dir, subpath);
+    // it seems that obsidian do not understand paths with backslashes in Windows, so turn them into forward slashes
+    return result.replace(/\\/g, "/");
 }
